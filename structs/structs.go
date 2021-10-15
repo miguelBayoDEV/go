f056ae1d97288d6e3e7f4adaac41709011dae418
miguelbayoDAW <miguelpuerta16@gmail.com>
2021-10-15 20:22:41 +0200

    Metodos

--- structs/structs.go
+++ structs/structs.go
@@ -9,6 +9,20 @@ type User struct {
 	name, lastName string
 }
 
+// Métodos. Su sintasis:
+func (usuario User) nombreCompleto() string {
+	return usuario.name + " " + usuario.lastName
+}
+
+// Para hacer un setter y que modifique el valor de una propiedad de struct se usa el puntero sino haría una copia
+func (this User) set_name(n string) {
+	this.name = n
+}
+
+func (this *User) setName(n string) {
+	this.name = n
+}
+
 func main() {
 	var mike User
 
@@ -27,9 +41,21 @@ func main() {
 
 	// Structs son mutables por lo que se puede variar los valores de los campos después de instanciarlos
 	mig.name = "Mig"
+	mig.lastName = "Bay"
 	(*mig).age = 25
 
 	fmt.Println(mig)
 	fmt.Println(mig.name)
 	fmt.Println((*mig).age)
+
+	// Imprimir método
+	fmt.Println(mig.nombreCompleto())
+
+	// No modifica nada porque hace una copia
+	mig.set_name("Door")
+	fmt.Println(mig.name)
+
+	// Al ser un puntero se imprime la modificación
+	mig.setName("Jeremo")
+	fmt.Println(mig.name)
 }
